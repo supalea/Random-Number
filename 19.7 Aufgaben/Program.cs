6f70da80786e2f6f7f0c64260ad4cf7237b68df4
Lea Schöllkopf <lea.schoellkopf@gmail.com>
2024-01-18 14:20:53 +0100

    Erste Version

---
+++ 19.7 Aufgaben/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random r = new Random();
+Dictionary <int, int> dict = new Dictionary <int, int>();
+int x;
+
+for (int i = 0; i < 10; i++)
+{
+     x = r.Next(0, 10);
+    Console.WriteLine($"{i} | {x}");
+
+    if (dict.ContainsKey(x))
+    {
+        dict[x]++;
+    }
+    else
+    {
+        dict[x] = 1;
+    }
+}
+
+foreach(var zahl in dict)
+{
+    Console.WriteLine("Zahl: {0}, Anzahl: {1}", zahl.Key, zahl.Value);
+}
+    
+
+
+
